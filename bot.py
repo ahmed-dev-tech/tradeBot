11cd1abb7cf335a9b50c9123e865ceeeaaa55fa9
ahmed-dev-tech <76089213+ahmed-dev-tech@users.noreply.github.com>
2022-03-22 23:47:44 +0500

    telegram integration

--- bot.py
+++ bot.py
@@ -1,6 +1,12 @@
+# import os
 import websocket, json, pprint,talib, numpy
-
-SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
+# import config
+# from binance.client import Client
+# from binance.enums import *
+# import telebot
+# TELE_API_KEY = os.getenv('API_KEY')
+# bot = telebot.TeleBot(TELE_API_KEY)
+SOCKET = "wss://stream.binance.com:9443/ws/btcusdt@kline_1m"
 
 RSI_PERIOD = 14
 RSI_OVERBOUGHT = 70
@@ -11,17 +17,17 @@ TRADE_QUANTITY = 0.05
 closes = []
 in_position = False
 
-client = Client(config.API_KEY, config.API_SECRET, tld='us')
+# client = Client(config.API_KEY, config.API_SECRET, tld='us')
 
-def order(side, quantity, symbol,order_type=ORDER_TYPE_MARKET):
-    try:
-        print("sending order")
-        order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
-        print(order)
-    except Exception as e:
-        print("an exception occured - {}".format(e))
-        return False
-    return True
+# def order(side, quantity, symbol,order_type=ORDER_TYPE_MARKET):
+#     try:
+#         print("sending order")
+#         order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
+#         print(order)
+#     except Exception as e:
+#         print("an exception occured - {}".format(e))
+#         return False
+#     return True
 
 def on_open(ws):
     print('opened connection')
@@ -55,10 +61,14 @@ def on_message(ws, message):
                 if in_position:
                     print("Overbought! Sell! Sell! Sell!")
                     # put binance sell logic here
-                    # order_succeeded = order(SIDE_SELL, TRADE_QUANTITY, TRADE_SYMBOL)
-                    # if order_succeeded:
-                    #     in_position = False
+                    order_succeeded = order(SIDE_SELL, TRADE_QUANTITY, TRADE_SYMBOL)
+                    if order_succeeded:
+                        in_position = False                                            
                     # put telegram sell signal logic here
+                    @bot.message_handler(func=order_succeeded)                   
+                    def sendSellSignal(message):
+                        bot.send_message(message.chat.id, "Sell")
+
                 else:
                     print("It is overbought, but we don't own any. Nothing to do.")
             
@@ -68,10 +78,13 @@ def on_message(ws, message):
                 else:
                     print("Oversold! Buy! Buy! Buy!")
                     # put binance buy order logic here
-                    # order_succeeded = order(SIDE_BUY, TRADE_QUANTITY, TRADE_SYMBOL)
-                    # if order_succeeded:
-                    #     in_position = True
+                    order_succeeded = order(SIDE_BUY, TRADE_QUANTITY, TRADE_SYMBOL)
+                    if order_succeeded:
+                        in_position = True
                     # put telegram buy signal logic here
-
+                    @bot.message_handler(func=order_succeeded)                   
+                    def sendBuySignal(message):
+                        bot.send_message(message.chat.id, "BUY")
+# bot.polling()
 ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
 ws.run_forever()
